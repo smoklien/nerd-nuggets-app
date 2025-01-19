996f3f9ece7e332d0dfe4ff03ac87350caf515e1
glib05 <119256208+glib05@users.noreply.github.com>
2025-01-19 22:38:19 +0200

    add saved endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('search/', views.PublicationListView.as_view(), name='search_publications'),
     path('pub/', views.PublicationView.as_view(), name='publication'),
     path('rec/', views.PersonalizationView.as_view(), name='recommendations'),
+    path('saved/', views.SavedView.as_view(), name='saved'),
 ]
